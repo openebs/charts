a653678c4578f4cd3e2133e29fa1b9dfaeac0949
Vitta <vitta@mayadata.io>
2020-03-17 11:02:42 +0530

    print debug messages to jiva_preupgrade_check.out
    Signed-off-by: Vitta <vitta@mayadata.io>

--- scripts/jiva-tools/jiva_preupgrade_checks.sh
+++ scripts/jiva-tools/jiva_preupgrade_checks.sh
@@ -36,23 +36,25 @@ function createFragOut() {
 	fi
 }
 
-#echo "volume.meta"
-#cat volume.meta
+echo "volume.meta" > jiva_preupgrade_check.out
+cat volume.meta >> jiva_preupgrade_check.out
 next=$(getAttr volume "Parent" 5)
 head=$(getAttr volume "Head" 2)
-#du -s $head
+echo "$head" >> jiva_preupgrade_check.out
+du -s "$head" >> jiva_preupgrade_check.out
 size=$(du -s "$head" | awk '{print $1}')
 chainLen=1
-#echo "$head"
 createFragOut "$head" $chainLen
 
 while [ "$next" != "" ]
 do
 	chainLen=$((chainLen + 1))
 	createFragOut "$next" $chainLen
-	#echo "$next.meta"
-	#du -s "$next"
-	#cat "$next.meta"
+	{
+		echo "$next.meta"
+		du -s "$next"
+		cat "$next.meta"
+	} >> jiva_preupgrade_check.out
 	img_size=$(du -s "$next" | awk '{print $1}')
 	size=$((size + img_size))
 	next=$(getAttr "$next" "Parent" 2)
@@ -60,25 +62,32 @@ done
 
 echo "Total used size: $size"
 
-echo "ChainLen:$chainLen"
+echo "Chain Length: $chainLen"
 
-#find . -iname '*.img'
 imgCnt=$(find . -iname '*.img' | wc -l)
+echo "img files count: $imgCnt"
 
-echo "img files count:$imgCnt"
-
-#find . -iname '*.img.meta'
 metaCnt=$(find . -iname '*.img.meta' | wc -l)
-
 echo "meta files count:$metaCnt"
 
+{
+	echo "Total used size: $size"
+	echo "Chain Length: $chainLen"
+
+	ls -ltr
+	find . -exec du -s {} \;
+
+	echo "img files count: $imgCnt"
+	echo "meta files count:$metaCnt"
+} >> jiva_preupgrade_check.out
+
 if [ $chainLen -ne "$imgCnt" ]; then
-	echo "chainLen and imgCnt doesn't match"
+	echo "Chain Length does not match with available img files"
 	exit 1
 fi
 
 if [ $chainLen -ne "$metaCnt" ]; then
-	echo "ChainLen and metaCnt doesn't match"
+	echo "Chain Length does not match with available meta files"
 	exit 1
 fi
 
